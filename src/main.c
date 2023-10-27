3a6cd1c0dc5cf685befeb1358ea4a34e8f418c2d
Corust <siraopob.dar@gmail.com>
2023-10-27 17:04:03 +0700

    add converter calendar and billCal initial file

--- src/main.c
+++ src/main.c
@@ -1,22 +1,36 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "calculator.c"
-
+#include "converter.c"
+#include "calendar.c"
 int main()
 {
-    char input = 0;
-    printf("Welcome ! please select which tool do you want to use :\n");
-    printf("1.calculator\n2.converter\n3.calendar\n4.bill\n5.exit\n");
-    printf("select option :");
-    scanf("%c", &input);
-    switch (input)
+    while (1)
     {
-    case '1':
-        calculatorPanel();
-        break;
-    case '2':
-        printf("2");
-        break;
-    default:
-        printf("Invalid option please select again.");
+        system("clear");
+        char input = 0;
+        printf("Select which tool do you want to use :\n");
+        printf("1.calculator\n2.converter\n3.calendar\n4.bill\n5.exit\n");
+        printf("select option :");
+        scanf("%c", &input);
+        switch (input)
+        {
+        case '1':
+            calculatorPanel();
+            break;
+        case '2':
+            converterPanel();
+            break;
+        case '3':
+            calendarPanel();
+            break;
+        case '4':
+            billCalculator();
+            break;
+        case '5':
+            exit(0);
+        default:
+            printf("Invalid option, please select again.");
+        }
     }
 }
