7810096a41d897e0edab517fd95e5197c9067039
Corust <siraopob.dar@gmail.com>
2023-10-28 23:40:34 +0700

    refactor calendar panel and method

--- src/calendar.c
+++ src/calendar.c
@@ -3,6 +3,7 @@
 
 int main();
 void refreshPanel();
+void calendarHeader();
 
 int get_1st_weekday(int year)
 {
@@ -13,15 +14,22 @@ int get_1st_weekday(int year)
 
 int calendarPanel()
 {
-    char input = 0;
-    int year, month, day, daysInMonth, weekDay = 0, startingDay;
-    printf("\nEnter year:");
-    scanf("%d", &year);
     while (1)
     {
-
+        char input = 0;
+        int year, month, day, daysInMonth, weekDay = 0, startingDay;
         char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         int monthDay[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        refreshPanel();
+        calendarHeader();
+        printf("How to use\n - enter which year you want to see the whole year calendar\n\n");
+        printf("Enter year : ");
+        scanf("%d", &year);
+
+        refreshPanel();
+        calendarHeader();
+        printf("\tCalendar of year %d\n", year);
 
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
             monthDay[1] = 29;
@@ -50,12 +58,26 @@ int calendarPanel()
                 startingDay = weekDay;
             }
         }
-        printf("\n\nPress 1 To Back To Main: ");
-        scanf("%c", &input);
+        printf("\n\n1.use tool again (input 1)\n2.back to main menu (input 2)\n3.exit (input 3)\n\n");
+        printf("select your option : ");
+        scanf("%s", &input);
         if (input == '1')
         {
-            refreshPanel();
-            break;
+            continue;
+        }
+        if (input == '2')
+        {
+            main();
+        }
+        if (input == '3')
+        {
+            printf("program exited.");
+            exit(0);
         }
     }
+}
+
+void calendarHeader()
+{
+    printf("-------------------- CALENDAR TOOL --------------------\n\n");
 }
